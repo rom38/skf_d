a002d8602392a7852f8182623fb82c04bcc5e7fb
Rman vsc <rom38@users.noreply.github.com>
2022-09-23 08:32:01 +0000

    d26 done

--- models_tutor/mc_donalds/models.py
+++ models_tutor/mc_donalds/models.py
@@ -1,10 +1,32 @@
-from turtle import position
+from datetime import datetime
+
 from django.db import models
+
 
 # Create your models here.
 
 class Order(models.Model):
-    pass
+    time_in = models.DateTimeField(auto_now_add = True)
+    time_out = models.DateTimeField(null = True)
+    cost = models.FloatField(default = 0.0)
+    pickup = models.BooleanField(default = False)
+    complete = models.BooleanField(default = False)
+    stuff = models.ForeignKey('Stuff', on_delete = models.CASCADE)
+
+    products = models.ManyToManyField('Product', through = 'ProductOrder')
+
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:
+            seconds = (self.time_out - self.time_in).total_seconds()
+            return seconds // 60
+        seconds = (datetime.now() - self.time_in).total_seconds()
+        return seconds // 60
+
 
 
 class Product(models.Model):
@@ -29,7 +51,24 @@ class Stuff(models.Model):
     full_name = models.CharField(max_length=64)
     position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
     labor_contract = models.IntegerField()
+    def get_last_name(self):
+        return self.full_name.split(' ')[0]
 
 
 class ProductOrder(models.Model):
-    pass
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    order = models.ForeignKey(Order, on_delete = models.CASCADE)
+    _amount = models.IntegerField(default = 1, db_column = 'amount')
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @amount.setter
+    def amount(self, value):
+        self._amount = int(value) if value >= 0 else 0
+        self.save()
