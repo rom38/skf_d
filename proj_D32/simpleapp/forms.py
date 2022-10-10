6e206e9f3cdca1d03e64db1fced16dea33a1ad7b
Rman vsc <rom38@users.noreply.github.com>
2022-10-10 10:15:29 +0000

    d44 done

---
+++ proj_D32/simpleapp/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Product
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = [
+               'name',
+               'description',
+               'category',
+               'price',
+               'quantity',
+            ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        description = cleaned_data.get("description")
+        if description is not None and len(description) < 20:
+            raise ValidationError({
+                "description": "Описание не может быть менее 20 символов."
+            })
+
+        name = cleaned_data.get("name")
+        if name == description:
+            raise ValidationError(
+                "Описание не должно быть идентично названию."
+            )
+
+        return cleaned_data
--- proj_D32/simpleapp/views.py
+++ proj_D32/simpleapp/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 from django.views.generic import ListView, DetailView
 from .models import Product
+from .filters import ProductFilter
 
 def multiply(request):
     number = request.GET.get('number')
@@ -20,16 +21,24 @@ def multiply(request):
 
 
 class ProductsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Product
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = 'name'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'products.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+    paginate_by = 2
+
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
     def get_context_data(self, **kwargs):
         # С помощью super() мы обращаемся к родительским классам
         # и вызываем у них метод get_context_data с теми же аргументами,
@@ -42,7 +51,9 @@ class ProductsList(ListView):
         # чтобы на её примере рассмотреть работу ещё одного фильтра.
         # context['next_sale'] = None
         context['next_sale'] = "Распродажа в среду!"
+        context['filterset'] = self.filterset
         return context
+
 
 class ProductDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
