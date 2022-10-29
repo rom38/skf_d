e329dfaec1c2a3687c9f560210b96b5ab6ecb87e
Rman vsc <rom38@users.noreply.github.com>
2022-10-29 13:28:58 +0000

    task d74 start

--- models_tutor/mc_donalds/views.py
+++ models_tutor/mc_donalds/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.views import View
+from .tasks import hello
+
+
+class IndexView(View):
+    def get(self, request):
+        hello.delay()
+        return HttpResponse('Hello!')
---
+++ news_portal/news_portal/celery.py
@@ -0,0 +1,9 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_portal.settings')
+
+app = Celery('news_portal')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
